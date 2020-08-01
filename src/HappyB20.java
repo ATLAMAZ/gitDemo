62884c6bf7b420cfd704b0e69ec4da931324d55f
Ulku Firat Atlamaz <38175135+ATLAMAZ@users.noreply.github.com>
2020-08-01 16:34:33 -0400

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,6 +1,6 @@
 public class HappyB20 {
     public static void main(String[] args) {
-        System.out.println("Happy B20, not happy any more");
+        System.out.println(" We are B20 Batch and we are happy");
 
     }
 }
